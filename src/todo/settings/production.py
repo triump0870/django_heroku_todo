49dfc97141ff5dac69ef7523a5450ba6795f71a0
Rohan Roy <triump0870@gmail.com>
2016-02-08 14:52:50 +0530

    Updated

--- src/todo/settings/production.py
+++ src/todo/settings/production.py
@@ -8,7 +8,7 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 # Must mention ALLOWED_HOSTS in production!
-# ALLOWED_HOSTS = ["todo.com"]
+ALLOWED_HOSTS = ["*"]
 
 # Cache the templates in memory for speed-up
 loaders = [
